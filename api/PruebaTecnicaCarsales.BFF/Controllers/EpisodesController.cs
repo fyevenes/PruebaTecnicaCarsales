be62f5a40963ba652d512c5f014dd74610daa2b9
Fernando yevenes <fdo.yevenes@gmail.com>
2025-12-04 13:05:43 -0300

    correcion try catch y busqueda por nombre

--- api/PruebaTecnicaCarsales.BFF/Controllers/EpisodesController.cs
+++ api/PruebaTecnicaCarsales.BFF/Controllers/EpisodesController.cs
@@ -17,9 +17,47 @@ namespace PruebaTecnicaCarsales.BFF.Controllers
         [HttpGet]
         public async Task<IActionResult> GetEpisodes([FromQuery] int page = 1)
         {
-            var episodes = await _service.GetEpisodesAsync(page);
-            if (episodes == null) return StatusCode(500, "Error al consumir la API externa");
-            return Ok(episodes);
+            try
+            {
+            var response = await _service.GetEpisodesAsync(page);
+            if (response == null) return StatusCode(500, "Error al consumir la API externa");
+            return Ok(response);
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode(503, $"Error al consumir API externa: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno: {ex.Message}");
+            }
+
         }
+
+        [HttpGet("search")] 
+        public async Task<IActionResult> SearchEpisodes([FromQuery] string query)
+        {
+           if (string.IsNullOrWhiteSpace(query)){ return BadRequest("Debe especificar un término de búsqueda.");}          
+                try
+                {
+                    var response = await _service.SearchEpisodes(query);
+                    if (response == null || response.Results == null || !response.Results.Any())
+                        return NotFound($"No se encontraron episodios con el término '{query}'.");
+
+                    return Ok(response);
+                }
+                catch (HttpRequestException ex)
+                {
+                    return StatusCode(503, $"Error al consumir API externa: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, $"Error interno: {ex.Message}");
+                }
+
+
+        }
+
+
     }
 }
--- api/PruebaTecnicaCarsales.BFF/Services/EpisodesServices.cs
+++ api/PruebaTecnicaCarsales.BFF/Services/EpisodesServices.cs
@@ -13,7 +13,7 @@ namespace PruebaTecnicaCarsales.BFF.Services
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(config["RickAndMortyApi:BaseUrl"]!);
         }
-
+         // busqueda de episodios por paginacion
         public async Task<EpisodeResponse?> GetEpisodesAsync(int page = 1)
         {
             var response = await _httpClient.GetAsync($"episode?page={page}");
@@ -23,5 +23,18 @@ namespace PruebaTecnicaCarsales.BFF.Services
             return JsonSerializer.Deserialize<EpisodeResponse>(content,
                 new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         }
+        // busqueda de episodios por nombre
+        public async Task<EpisodeResponse?> SearchEpisodes(string query)
+        {
+            var response = await _httpClient.GetAsync($"episode/?name={query}");
+    
+            if (!response.IsSuccessStatusCode) return null;
+
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+            return JsonSerializer.Deserialize<EpisodeResponse>(content,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+  
     }
 }
